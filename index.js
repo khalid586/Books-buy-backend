39d2915b2bd7b892219a82481bd1988eadfc11a9
khalid586 <khalidcomilla58@gmail.com>
2024-05-18 18:51:08 +0600

    connected to frontend

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 
 
 const app = express();
 const port = process.env.PORT || 5007;
+
+const corsOptions = {
+    origin: [ 
+        'http://localhost:5173',
+    ],
+    optionsSuccessStatus: 200 
+  };
+  
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+
+app.post('/add_book',(req,res)=>{
+    const data = req.body;
+    console.log(data)
+})
 
 app.get('/',(req,res)=>{
     res.send('This is working !!!!')
